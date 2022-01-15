941a80d516df5e820a959b099ecf8b726b094b74
shivamdargan <darganshivam2001@gmail.com>
2022-01-15 19:46:23 +0530

    Changes Regarding Hero Section And Card Boundaries

--- assets/js/heroText.js
+++ assets/js/heroText.js
@@ -1,20 +1,51 @@
-document
-  .querySelector('h7')
-  .addEventListener('mousemove', ({target, clientX, clientY}) => {
-    const {x,y} = getQuadrants(target, clientX, clientY)
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['Hello World', 'HackOwasp 4.0','Is On','25-27 Feburary'], 'text',['tomato','rebeccapurple','blue','rebeccapurple']);
 
-    target.attributeStyleMap.set('--x-quadrant', `${x}`)
-    target.attributeStyleMap.set('--y-quadrant', `${y}`)
-  })
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
 
-const getQuadrants = (element, clientX, clientY) => {
-  const { x, y, width, height } = element.getBoundingClientRect()
-  
-  const quadX = clientX - (x + 0.5 * width)
-  const quadY = clientY - (y + 0.5 * height)
-  
-  return {
-    x: quadX >= 0 ? 1 : -1, 
-    y: quadY >= 0 ? 1 : -1,
-  }
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
 }
