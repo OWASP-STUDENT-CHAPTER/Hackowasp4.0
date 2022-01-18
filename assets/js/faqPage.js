3ad20cb3e1edd4724cbb7437ecfa5b9e96840428
jogeshgupta963 <jogeshgupta963@gmail.com>
2022-01-18 18:39:12 +0530

    added:hover and click on button

--- assets/js/faqPage.js
+++ assets/js/faqPage.js
@@ -6,8 +6,8 @@ const ansBar = document.querySelector(".answer-box")
 const qBar=document.querySelector(".question-box");
 
 question[0].value={
-   q:"This is q1",
-   a:"This is a1"
+   q:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores voluptas, deserunt repudiandae sequi quod amet. Ducimus libero dolorem tempora, nemo consectetur quos. Sequi quam officiis, blanditiis dignissimos praesentium quas dolore?",
+   a:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores voluptas, deserunt repudiandae sequi quod amet. Ducimus libero dolorem tempora, nemo consectetur quos. Sequi quam officiis, blanditiis dignissimos praesentium quas dolore?"
 }
 question[1].value={
    q:"This is q2",
@@ -29,18 +29,23 @@ question[5].value={
    q:"This is q6",
    a:"this is a6"
 }
-
+var c=0;
 function print(i){
 
-
-    ansBar.innerText = question[i].value.a;
-    qBar.innerText = question[i].value.q;
+   // question[i].style.backgroundColor="red";
+   question[c].classList.remove("colorClass")
+  
+   c=i;
+   question[i].classList.add("colorClass")
+   ansBar.innerText = question[i].value.a;
+   qBar.innerText = question[i].value.q;
    
 }
 
 for(let i=0;i<6;i++){
-
-    question[i].addEventListener("click",()=>{
+   
+   question[i].addEventListener("click",()=>{
+            
         print(i);
     })
 }
