a743e07f7865d3ae78991fe053d56ccfa01be11e
Aman Kumar <aman.kumar11112001@gmail.com>
2022-01-31 18:14:37 +0530

    fix

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,7 +1,7 @@
 window.addEventListener("load",()=>{
 
     
-    // document.querySelector(".loader").style.display="none"
+    document.querySelector(".loader").style.display="none";
     document.querySelector(".loader").style.opacity=0;
     document.querySelector(".loader").style.zIndex=0;
     
