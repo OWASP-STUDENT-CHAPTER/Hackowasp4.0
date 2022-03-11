a532a8031550b0d2835e0ecc682534133f63b54b
Nikhil Bakshi <79579230+bonvagnik@users.noreply.github.com>
2022-03-11 22:12:17 +0530

    Added MLH prizes

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 11, 2022 22:00:00").getTime();
+var countDownDate = new Date("Mar 11, 2022 23:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
